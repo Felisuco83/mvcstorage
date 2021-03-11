b9f6e0dbce0e740e81c7d1129e3d23445b5b954b
Mataguiris Palotes <felixwebdev@gmail.com>
2021-03-11 12:33:56 +0100

    cambios 11 noviembre

---
+++ MvcStorageFile/Controllers/AlumnosController.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using MvcStorage.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MvcStorage.Controllers
+{
+    public class AlumnosController : Controller
+    {
+        ServiceTableAlumnos ServiceAlumnos;
+        public AlumnosController(ServiceTableAlumnos service)
+        {
+            this.ServiceAlumnos = service;
+        }
+        public IActionResult Index()
+        {
+            return View();
+        }
+
+        [HttpPost]
+        public async Task<IActionResult> Index (string curso)
+        {
+            string token = await this.ServiceAlumnos.GetTokenAsync(curso);
+            return View(this.ServiceAlumnos.GetAlumnos(token));
+        }
+    }
+}
